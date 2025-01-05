0b6fb3f37cd40bb3457c15297fb06a1dd3023419
lambovg <georgi.lambov@gmail.com>
2025-01-05 16:38:10 +0200

    Add utility functions for testing in a new package
    Introduced a new `internal/testing` package with utility functions for common assertions like equality, regex matching, and nil checks. These functions aim to simplify and standardize test validations across the codebase.

---
+++ internal/testing/test.go
@@ -0,0 +1,39 @@
+package testing
+
+import (
+	"reflect"
+	"regexp"
+	"testing"
+)
+
+func Ok(t *testing.T, got string, want string) {
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func Equals(t *testing.T, got interface{}, want interface{}) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func Match(t *testing.T, got string, want string) {
+	m, _ := regexp.MatchString(want, got)
+
+	if !m {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func IsNull(t *testing.T, got interface{}) {
+	if got != nil {
+		t.Errorf("expected nil but got %v", got)
+	}
+}
+
+func IsNotNull(t *testing.T, got interface{}) {
+	if got == nil {
+		t.Errorf("expected not nil but got %v", got)
+	}
+}
