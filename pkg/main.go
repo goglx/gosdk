4a2905e793acc68db424d5f3ccec804aa92d804a
lambovg <georgi.lambov@gmail.com>
2025-01-05 14:53:41 +0200

    Add Debug function and main package for S3 testing
    This commit adds a Debug function in the S3 config to output the S3 URL for debugging purposes. Additionally, a main package is introduced to serve as an entry point for testing the S3 functionality.

---
+++ pkg/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"gosdk/pkg/storage/s3"
+)
+
+func main() {
+
+	fmt.Println("gosdk")
+	s3.Debug()
+}
--- pkg/storage/s3/config.go
+++ pkg/storage/s3/config.go
@@ -1,3 +1,9 @@
 package s3
 
+import "fmt"
+
 var s3URL = "https://%s.s3.%s.amazonaws.com/%s"
+
+func Debug() {
+	fmt.Println("s3 debug", s3URL)
+}
