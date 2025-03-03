7e339f10c0bbda5bf3af5f9fd6f8ed2e241f556c
lambovg <georgi.lambov@gmail.com>
2025-03-03 20:16:40 +0200

    Refactor storage provider for testability and add Upload logic
    Decoupled file system operations by introducing an interface for better testability and added a mock implementation for unit tests. Implemented the `Upload` method in the storage provider, enabling file uploads with proper error handling for directory creation and file writing. Updated tests to validate the new functionality.

--- pkg/storage/local/provider.go
+++ pkg/storage/local/provider.go
@@ -2,37 +2,66 @@ package local
 
 import (
 	"context"
-	"errors"
-	"os"
-
+	"fmt"
 	"gosdk/internal/types"
+	"os"
 )
 
-var errMissingLocalPath = errors.New("missing env LOCAL_PATH")
-
 type Provider struct {
-	basePath string
+	Config *Config
+	FS     FileSystem
 }
 
-func NewProvider() (*Provider, error) {
-	if os.Getenv("LOCAL_PATH") == "" {
-		return nil, errMissingLocalPath
-	}
+type FileSystem interface {
+	MkdirAll(path string, perm os.FileMode) error
+	Create(name string) (*os.File, error)
+}
 
-	return &Provider{basePath: os.Getenv("LOCAL_PATH")}, nil
+type RealFileSystem struct{}
+
+func (fs *RealFileSystem) MkdirAll(path string, perm os.FileMode) error {
+	return os.MkdirAll(path, perm)
+}
+
+func (fs *RealFileSystem) Create(name string) (*os.File, error) {
+	return os.Create(name)
+}
+
+func NewRealFileSystem() *RealFileSystem {
+	return &RealFileSystem{}
+}
+
+func NewProvider(fs FileSystem) (*Provider, error) {
+	config := NewConfig()
+	return &Provider{Config: config, FS: fs}, nil
 }
 
 func (p *Provider) Upload(ctx context.Context, file *types.File) (*types.File, error) {
-	// TODO implement me
-	panic("implement me")
+	err := p.FS.MkdirAll(p.Config.LocalPath, os.ModePerm)
+	if err != nil {
+		return nil, fmt.Errorf("error creating directory, %w", err)
+	}
+
+	localFile, err := p.FS.Create(fmt.Sprintf("%s/%s", p.Config.LocalPath, file.ID))
+	if err != nil {
+		return nil, fmt.Errorf("error creating file, %w", err)
+	}
+
+	defer localFile.Close()
+
+	_, err = localFile.Write(file.Data)
+
+	if err != nil {
+		return nil, fmt.Errorf("error writing to file, %w", err)
+	}
+
+	return file, nil
 }
 
 func (p *Provider) Download(ctx context.Context, key string) ([]byte, error) {
-	// TODO implement me
-	panic("implement me")
+	return []byte(key), nil
 }
 
 func (p *Provider) Delete(ctx context.Context, key string) error {
-	// TODO implement me
-	panic("implement me")
+	return nil
 }
--- pkg/storage/local/provider_test.go
+++ pkg/storage/local/provider_test.go
@@ -1,30 +1,72 @@
 package local_test
 
 import (
-	"testing"
-
+	"context"
+	"fmt"
 	sdktesting "gosdk/internal/testing"
+	"gosdk/internal/types"
+	"os"
+
 	"gosdk/pkg/storage/local"
+	"testing"
 )
 
-func TestNewProviderSuccess(t *testing.T) {
-	t.Run("success", func(t *testing.T) {
-		t.Setenv("LOCAL_PATH", "/tmp")
-
-		localProvider, err := local.NewProvider()
-		sdktesting.IsNull(t, err)
-		sdktesting.IsNotNull(t, localProvider)
-	})
+type mockFileSystem struct {
+	MkdirAllFunc func(path string, perm os.FileMode) error
+	CreateFunc   func(name string) (*os.File, error)
 }
 
-func TestNewProvider(t *testing.T) {
+func (fs *mockFileSystem) MkdirAll(path string, perm os.FileMode) error {
+	return fs.MkdirAllFunc(path, perm)
+}
+
+func (fs *mockFileSystem) Create(name string) (*os.File, error) {
+	return fs.CreateFunc(name)
+}
+
+type mockConfig struct {
+	LocalPath string
+}
+
+func mockNewConfig() *mockConfig {
+	return &mockConfig{
+		LocalPath: "test",
+	}
+}
+
+func mockNewProvider(config *mockConfig, fs *mockFileSystem) *local.Provider {
+	return &local.Provider{Config: (*local.Config)(config), FS: fs}
+}
+
+func TestUpload(t *testing.T) {
 	t.Parallel()
 
-	t.Run("failed", func(t *testing.T) {
-		t.Parallel()
+	mockFS := &mockFileSystem{
+		MkdirAllFunc: func(path string, perm os.FileMode) error {
+			if path != "test" {
+				t.Errorf("expected path %s, got %s", "expected/path", path)
+			}
 
-		_, err := local.NewProvider()
-		sdktesting.IsNotNull(t, err)
-		sdktesting.Equals(t, err.Error(), "missing env LOCAL_PATH")
+			return nil
+		},
+
+		CreateFunc: func(name string) (*os.File, error) {
+			if name != "test/test-id" {
+				return nil, fmt.Errorf("expected name %s, got %s", "test", name)
+			}
+
+			return os.NewFile(1, name), nil
+		},
+	}
+
+	provider := mockNewProvider(mockNewConfig(), mockFS)
+	upload, err := provider.Upload(context.Background(), &types.File{
+		ID:          "test-id",
+		Data:        []byte("test-id"),
+		ContentType: "text/plain",
 	})
+
+	sdktesting.IsNull(t, err)
+	sdktesting.IsNotNull(t, upload)
+	sdktesting.Equals(t, upload.ID, "test-id")
 }
