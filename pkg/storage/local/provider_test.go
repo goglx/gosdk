e608ebe27722bdfa4862d9ce0e02613144180ba0
lambovg <georgi.lambov@gmail.com>
2025-03-03 15:26:56 +0200

    Refactor provider tests by simplifying environment setup.
    Removed redundant environment variable checks and refactored the test setup using `t.Setenv` for cleaner and more concise code. This enhances readability and aligns with best practices for testing in Go.

--- pkg/storage/local/provider_test.go
+++ pkg/storage/local/provider_test.go
@@ -1,52 +1,29 @@
 package local_test
 
 import (
-	"errors"
-	"fmt"
-	"os"
 	"testing"
 
 	sdktesting "gosdk/internal/testing"
 	"gosdk/pkg/storage/local"
 )
 
-var errMissingEnvironmentVariable = errors.New("missing environment variable")
-
-func checkEnvVariables(provider string, variables []string) error {
-	for _, v := range variables {
-		if os.Getenv(v) == "" {
-			return fmt.Errorf("%w: %s provider, variable %s", errMissingEnvironmentVariable, provider, v)
-		}
-	}
-
-	return nil
-}
-
-func setupEnv() error {
-	return checkEnvVariables("local", []string{"LOCAL_PATH"})
-}
-
-func TestNewProvider(t *testing.T) {
-	t.Parallel()
-
+func TestNewProviderSuccess(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
-		t.Parallel()
-
-		err := os.Setenv("LOCAL_PATH", "/tmp")
-		sdktesting.IsNull(t, err)
+		t.Setenv("LOCAL_PATH", "/tmp")
 
 		localProvider, err := local.NewProvider()
 		sdktesting.IsNull(t, err)
 		sdktesting.IsNotNull(t, localProvider)
 	})
+}
+
+func TestNewProvider(t *testing.T) {
+	t.Parallel()
 
 	t.Run("failed", func(t *testing.T) {
 		t.Parallel()
 
-		err := os.Unsetenv("LOCAL_PATH")
-		sdktesting.IsNull(t, err)
-
-		_, err = local.NewProvider()
+		_, err := local.NewProvider()
 		sdktesting.IsNotNull(t, err)
 		sdktesting.Equals(t, err.Error(), "missing env LOCAL_PATH")
 	})
