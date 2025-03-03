5203663cbe9948738f09c04b92a7e98ffe09a30e
lambovg <georgi.lambov@gmail.com>
2025-03-03 13:24:02 +0200

    Add mock provider and upload tests for storage Manager
    Introduced a mock provider to simplify testing of storage Manager functionality. Added unit tests for the Upload method, covering both success and failure scenarios. These changes improve test isolation and coverage.

--- pkg/storage/provider_test.go
+++ pkg/storage/provider_test.go
@@ -1,6 +1,8 @@
 package storage_test
 
 import (
+	"context"
+	"errors"
 	"testing"
 
 	sdktesting "gosdk/internal/testing"
@@ -34,12 +36,12 @@ func TestNewProviderManager(t *testing.T) {
 		{
 			name:   types.Local,
 			want:   false,
-			errMsg: "failed to create local provider: not implemented",
+			errMsg: "failed to create local Provider: not implemented",
 		},
 		{
 			name:   "wrong",
 			want:   false,
-			errMsg: "unsupported provider type",
+			errMsg: "unsupported Provider type",
 		},
 	}
 
@@ -60,4 +62,82 @@ func TestNewProviderManager(t *testing.T) {
 			}
 		})
 	}
+}
+
+type mockProvider struct {
+	mockUpload   func(ctx context.Context, file *types.File) (*types.File, error)
+	mockDownload func(ctx context.Context, key string) ([]byte, error)
+	mockDelete   func(ctx context.Context, key string) error
+}
+
+func (mp *mockProvider) Upload(ctx context.Context, file *types.File) (*types.File, error) {
+	return mp.mockUpload(ctx, file)
+}
+
+func (mp *mockProvider) Download(ctx context.Context, key string) ([]byte, error) {
+	return mp.mockDownload(ctx, key)
+}
+
+func (mp *mockProvider) Delete(ctx context.Context, key string) error {
+	return mp.mockDelete(ctx, key)
+}
+
+// NewMock is a helper function to create a Manager with a mock Provider
+func newMock(mockProvider storage.Provider) (*storage.Manager, error) {
+	return &storage.Manager{Provider: mockProvider}, nil
+}
+
+func TestUpload(t *testing.T) {
+	t.Parallel()
+
+	t.Run("success", func(t *testing.T) {
+		t.Parallel()
+
+		provider := &mockProvider{
+			mockUpload: func(ctx context.Context, file *types.File) (*types.File, error) {
+				return file, nil
+			},
+			mockDelete:   nil,
+			mockDownload: nil,
+		}
+
+		manager, err := newMock(provider)
+		sdktesting.IsNull(t, err)
+		sdktesting.IsNotNull(t, manager)
+		sdktesting.IsNotNull(t, manager.Provider)
+
+		upload, err := manager.Upload(context.TODO(), &types.File{
+			ID:          "upload-id",
+			ContentType: "text/plain",
+			Data:        []byte("test-upload"),
+		})
+
+		sdktesting.IsNull(t, err)
+		sdktesting.IsNotNull(t, upload)
+		sdktesting.Equals(t, upload.ID, "upload-id")
+	})
+
+	t.Run("failed", func(t *testing.T) {
+		t.Parallel()
+
+		provider := &mockProvider{
+			mockUpload: func(ctx context.Context, file *types.File) (*types.File, error) {
+				return nil, errors.New("failed to upload")
+			},
+			mockDelete:   nil,
+			mockDownload: nil,
+		}
+
+		manager, err := newMock(provider)
+		sdktesting.IsNull(t, err)
+
+		_, err = manager.Upload(context.TODO(), &types.File{
+			ID:          "upload-id",
+			ContentType: "text/plain",
+			Data:        []byte("test-upload"),
+		})
+
+		sdktesting.IsNotNull(t, err)
+		sdktesting.Equals(t, err.Error(), "failed to upload file upload-id: failed to upload")
+	})
 }
