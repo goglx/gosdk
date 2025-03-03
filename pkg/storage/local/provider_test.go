243fe712e7ff195470512408e18892552540d856
lambovg <georgi.lambov@gmail.com>
2025-03-03 14:30:46 +0200

    Add parallel execution for subtests in provider tests
    Enable subtests within `TestNewProvider` to run in parallel, improving test performance and isolation. Also, include a check for specific error messages in the "failed" test case.

--- pkg/storage/local/provider_test.go
+++ pkg/storage/local/provider_test.go
@@ -1,22 +1,28 @@
 package local_test
 
 import (
+	"testing"
+
 	sdktesting "gosdk/internal/testing"
 	"gosdk/pkg/storage/local"
-	"testing"
 )
 
 func TestNewProvider(t *testing.T) {
 	t.Parallel()
 
 	t.Run("success", func(t *testing.T) {
+		t.Parallel()
+
 		localProvider, err := local.NewProvider()
 		sdktesting.IsNull(t, err)
 		sdktesting.IsNotNull(t, localProvider)
 	})
 
 	t.Run("failed", func(t *testing.T) {
+		t.Parallel()
+
 		_, err := local.NewProvider()
 		sdktesting.IsNotNull(t, err)
+		sdktesting.Equals(t, err.Error(), "not implemented")
 	})
 }
