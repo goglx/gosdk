6f32337760cec31ea2a7dac2bb386c89f9788ede
lambovg <georgi.lambov@gmail.com>
2025-03-16 00:54:28 +0200

    Add file upload logic using local storage provider
    Implemented a local storage provider to handle file uploads and added a sample usage in the `main.go`. This includes setting up the local directory, testing uploads, and logging file IDs for verification.

--- main.go
+++ main.go
@@ -1,3 +1,50 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"gosdk/internal/types"
+	"gosdk/pkg/storage"
+	"gosdk/pkg/storage/local"
+)
+
+func main() {
+	err := os.Setenv("LOCAL_PATH", "./")
+	if err != nil {
+		return
+	}
+
+	provider, err := local.NewProvider()
+	if err != nil {
+		return
+	}
+
+	file, err := provider.Upload(context.TODO(), &types.File{
+		ID:          "test-id",
+		Data:        []byte("test-id"),
+		ContentType: "text/plain",
+	})
+	if err != nil {
+		return
+	}
+
+	slog.Info(file.ID)
+
+	manager, err := storage.New(types.Local)
+	if err != nil {
+		return
+	}
+
+	upload, err := manager.Upload(context.TODO(), &types.File{
+		ID:          "test-id-2",
+		Data:        []byte("test-id"),
+		ContentType: "text/plain",
+	})
+	if err != nil {
+		return
+	}
+
+	slog.Info(upload.ID)
+}
