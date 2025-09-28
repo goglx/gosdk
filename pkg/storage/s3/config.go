a34134a8e8ace330d8e12171f2ef9a30bc2a4c60
lambovg <georgi.lambov@gmail.com>
2025-09-28 16:10:57 +0300

    refactor(pkg/storage/s3): move client.go to config.go and add Config struct with environment variable support

--- pkg/storage/s3/config.go
+++ pkg/storage/s3/config.go
@@ -1,8 +1,47 @@
 package s3
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 const s3URL = "https://%s.s3.%s.amazonaws.com/%s"
+
+type Config struct {
+	bucket    string
+	accessKey string
+	secretKey string
+	region    string
+}
+
+func NewConfig() *Config {
+	bucket := os.Getenv("BUCKET_NAME")
+	if bucket == "" {
+		panic("missing env BUCKET_NAME")
+	}
+
+	region := os.Getenv("S3_REGION")
+	if region == "" {
+		panic("missing env S3_REGION")
+	}
+
+	accessKey := os.Getenv("S3_ACCESS_KEY")
+	if accessKey == "" {
+		panic("missing env S3_ACCESS_KEY")
+	}
+
+	secretKey := os.Getenv("S3_SECRET_KEY")
+	if secretKey == "" {
+		panic("missing env S3_SECRET_KEY")
+	}
+
+	return &Config{
+		bucket,
+		region,
+		accessKey,
+		secretKey,
+	}
+}
 
 func debug() string {
 	return fmt.Sprintf(s3URL, "1", "2", "3")
